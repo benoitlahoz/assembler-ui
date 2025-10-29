e795dceb9ff2cc5dea80a162a42be48cb7299c46
Benoît LAHOZ <lahoz.ben@gmail.com>
2025-10-29 13:48:38 +0100

    LLMS first version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,53 @@ export default defineNuxtConfig({
   },
   modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/scripts', '@nuxt/ui'],
   robots: { robotsTxt: false },
+  llms: {
+    domain: 'https://benoitlahoz.github.io/assembler-ui',
+    title: 'Assembler UI',
+    description: 'Documentation for AssemblerUI components and features',
+    sections: [
+      {
+        title: 'Getting Started',
+        description: 'Getting started with Assembler UI',
+        links: [
+          {
+            title: 'Introduction',
+            description: 'Project overview',
+            href: '/getting-started/introduction',
+          },
+          {
+            title: 'Installation',
+            description: 'How to install Assembler UI',
+            href: '/getting-started/installation',
+          },
+          {
+            title: 'Project Structure',
+            description: 'File organization',
+            href: '/getting-started/project-structure',
+          },
+        ],
+      },
+      {
+        title: 'Essentials',
+        description: 'Core features and syntax',
+        links: [
+          {
+            title: 'Markdown Syntax',
+            description: 'Markdown syntax guide',
+            href: '/essentials/markdown-syntax',
+          },
+          {
+            title: 'Code Blocks',
+            description: 'How to use code blocks',
+            href: '/essentials/code-blocks',
+          },
+          {
+            title: 'Components',
+            description: 'The list ofcomponents',
+            href: '/essentials/components',
+          },
+        ],
+      },
+    ],
+  },
 });
